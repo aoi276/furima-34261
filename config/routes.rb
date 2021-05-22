c627d4b41dbf3ad44b3cf48cab806bf9daf4d4a9
aoi276 <79250848+aoi276@users.noreply.github.com>
2021-05-23 08:12:56 +0900

    商品編集ページに遷移実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'items/index'
   get 'items/new'
   root to: 'items#index'
-  resources :items, only:[:index, :new, :creat, :show]
+  resources :items, only:[:index, :new, :creat, :show, :edit]
   devise_for :users
 end
