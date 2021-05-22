b9e6b3c067909d1b5656f071e5ba6b317d6e9e34
aoi276 <79250848+aoi276@users.noreply.github.com>
2021-05-22 14:24:47 +0900

    商品詳細画面に遷移

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'items/index'
   get 'items/new'
   root to: 'items#index'
-  resources :items, only:[:index, :new, :create]
+  resources :items, only:[:index, :new, :creat, :show]
   devise_for :users
 end
