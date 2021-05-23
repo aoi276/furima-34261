ba0949bd5c546d630e5a7ca5c21afa2711452d32
aoi276 <79250848+aoi276@users.noreply.github.com>
2021-05-23 09:41:45 +0900

    商品情報編集機能修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,7 +44,7 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    unless current_user.id == @item.user_id
+    unless current_user == @item.user
       redirect_to action: :index
     end
   end
