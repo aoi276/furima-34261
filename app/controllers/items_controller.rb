fbb01374709ec2a252b0ea31a7050d061086de10
aoi276 <79250848+aoi276@users.noreply.github.com>
2021-05-23 17:46:47 +0900

    商品情報編集機能修正5

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
  before_action :authenticate_user!, only:[:new, :create, :edit]
- before_action :set_item, only: [:show, :edit]
+ before_action :set_item, only: [:show, :edit, :update]
  before_action :move_to_index, only: [:edit, :update]
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -26,7 +27,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_params
@@ -46,7 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     unless current_user == @item.user
       redirect_to action: :index
     end
