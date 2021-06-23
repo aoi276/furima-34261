15facbfca58ff82d04eaf382ad37461fbedb7492
aoi276 <79250848+aoi276@users.noreply.github.com>
2021-06-23 19:59:46 +0900

    新規複数枚画像投稿機能実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -3,22 +3,34 @@ document.addEventListener('DOMContentLoaded', function() {
   const ImageList = document.getElementById('image-list');
 
   const createImageHTML = (blob) => {
-   const imageElement = document.createElement('div');
+   const imageElement = document.createElement('div')
+    imageElement.setAttribute('class', "image-element")
+    let imageElementNum = document.querySelectorAll('.image-element').length
 
    const blobImage = document.createElement('img');
    blobImage.setAttribute('src', blob);
-   imageElement.appendChild(blobImage);
-   ImageList.appendChild(imageElement);
- };
+   blobImage.setAttribute('class', "photo");
 
-  document.getElementById('item-image').addEventListener('change', function(e) {
-     const imageContent = document.querySelector('img');
-     if (imageContent) {
-      imageContent.remove();
-     }
+   const inputHTML = document.createElement('input')
+   inputHTML.setAttribute('id', `message_image_${imageElementNum}`)
+   inputHTML.setAttribute('name', 'message[images[]')
+   inputHTML.setAttribute('type', 'file')
 
-     const file = e.target.files[0];
-     const blob = window.URL.createObjectURL(file);
+   imageElement.appendChild(blobImage)
+   imageElement.appendChild(inputHTML)
+   ImageList.appendChild(imageElement)
+
+   inputHTML.addEventListener('change', (e) => {
+     file = e.target.files[0];
+     blob = window.URL.createObjectURL(file);
+
+     createImageHTML(blob)
+   })
+ }
+
+  document.getElementById('item-image').addEventListener('change', (e) => {
+     let file = e.target.files[0];
+     let blob = window.URL.createObjectURL(file);
 
     createImageHTML(blob);
   });
