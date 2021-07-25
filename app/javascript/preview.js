e2fcf8de4baab7290bad7bbefe19b86e9e77505c
aoi276 <79250848+aoi276@users.noreply.github.com>
2021-07-25 18:02:49 +0900

    Revert "Revert "出品画面8枚表示""
    This reverts commit 50f1d00571dbe8db4957c5ea08b1a527e4181610.

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,38 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() {
- if ( document.getElementById('item-image')) {
-  const ImageList = document.getElementById('image-list');
+  if ( document.getElementById('item-image')) {
+    const ImageList = document.getElementById('image-list')
+    ImageList.setAttribute('class','image-box')
 
-  const createImageHTML = (blob) => {
-   const imageElement = document.createElement('div')
-    imageElement.setAttribute('class', "image-element")
-    let imageElementNum = document.querySelectorAll('.image-element').length
+    // 選択した画像を表示する関数
+    const createImageHTML = (blob) => {
+      // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement('div')
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length
 
-   const blobImage = document.createElement('img');
-   blobImage.setAttribute('src', blob);
-   blobImage.setAttribute('class', "photo");
+      // 8枚目以降は表示しない
+      if (imageElementNum == 8) {
+        blobImage.remove()
+        inputHTML.remove()
+      }
 
-   const inputHTML = document.createElement('input')
-   inputHTML.setAttribute('id', `message_image_${imageElementNum}`)
-   inputHTML.setAttribute('name', 'message[images[]')
-   inputHTML.setAttribute('type', 'file')
+      // ファイル選択ボタンを生成
+      const blobImage = document.createElement('img')
+      blobImage.setAttribute('src', blob);
+      blobImage.setAttribute('class', "photo")
 
-   imageElement.appendChild(blobImage)
-   imageElement.appendChild(inputHTML)
-   ImageList.appendChild(imageElement)
+      // 生成したHTMLの要素をブラウザに表示させる
+      const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `item_image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'item[images][]')
+      inputHTML.setAttribute('type', 'file')
 
-   inputHTML.addEventListener('change', (e) => {
-     file = e.target.files[0];
-     blob = window.URL.createObjectURL(file);
+      imageElement.appendChild(blobImage)
+      imageElement.appendChild(inputHTML)
+      ImageList.appendChild(imageElement)
 
-     createImageHTML(blob)
-   })
- }
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
 
-  document.getElementById('item-image').addEventListener('change', (e) => {
-     let file = e.target.files[0];
-     let blob = window.URL.createObjectURL(file);
+        createImageHTML(blob)
+      })
+    }
 
-    createImageHTML(blob);
-  });
- }
+    document.getElementById('item-image').addEventListener('change', (e) => {
+      let file = e.target.files[0];
+      let blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob)
+    });
+  }
 });
